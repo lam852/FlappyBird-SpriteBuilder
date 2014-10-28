23d4f7ceb71bf1ad8026fdcd0e2fb2ee59ad6925
Lam <lam852@gmail.com>
2014-10-28 15:29:10 -0400

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -15,6 +15,18 @@
 -(void)update:(CCTime)delta
 {
     // put update code here
+    //Increment the time since the last obstacle was added
+    timeSinceObstacle += delta; // delta is apporximately 1/60th of a second
+    // Check to see if two sconds have passed
+    if (timeSinceObstacle > 2.0f)
+    {
+        // Add a new obstacle
+        [self addObstacle];
+        
+        // Then resent the timer.
+        timeSinceObstacle = 0.0f;
+    
+    }
 }
 
 // put new methods here
